708e361e7648da5ec9e81e2b1cc92a61e6b5956b
negadras <negamdras@gmail.com>
2025-06-04 18:01:10 +0200

    feat(quote): implement random quote command

---
+++ cmd/quote.go
@@ -0,0 +1,32 @@
+package cmd
+
+import (
+	"github.com/spf13/cobra"
+	"math/rand"
+)
+
+// Quote when the quote command is used it should return randomly a quote from the list
+// should this quote be in a json so that we can manage them edit/update/delete
+var Quote = []string{
+	"Shower thoughts only work when you put in the work.",
+	"Take the time it takes so it takes less time.",
+	"Don't go out and try to find the quotes, they should come to you.",
+	"Nobody cares how hard you worked, only the results.",
+	"If you want everything to be familiar, you will never learn anything new.",
+	"Do or do not. There is no try.",
+}
+
+func newQuoteCommand() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "quote",
+		Short: "Tada random quote from our list",
+		RunE: func(cmd *cobra.Command, args []string) error {
+			randomQuote := Quote[rand.Intn(len(Quote))]
+
+			cmd.Printf("\n 💬 %s\n", randomQuote)
+			return nil
+		},
+	}
+
+	return cmd
+}
---
+++ cmd/root.go
@@ -0,0 +1,35 @@
+package cmd
+
+import (
+	"fmt"
+	"github.com/fatih/color"
+	"github.com/spf13/cobra"
+	"os"
+)
+
+func newRootCommand() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "tada",
+		Short: "A command line todo app",
+		Long: `tada is a CLI that will help you add todo list, list your 
+todo list, edit, close ...`,
+		SilenceErrors: true,
+		PersistentPreRun: func(cmd *cobra.Command, args []string) {
+			cmd.SilenceUsage = true
+		},
+	}
+
+	cmd.AddCommand(newQuoteCommand())
+
+	return cmd
+}
+
+func Execute() {
+	cmd := newRootCommand()
+
+	err := cmd.Execute()
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "%s %v\n", color.RedString("error:"), err)
+		os.Exit(1)
+	}
+}

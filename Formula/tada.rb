b95c327d506ee6dc17a99674c1072d9f86b46cc0
negadras <negadras@users.noreply.github.com>
2025-06-04 21:21:08 +0000

    chore(formula): bump tada to 0.0.2

--- Formula/tada.rb
+++ Formula/tada.rb
@@ -6,7 +6,7 @@ class Tada < Formula
   # Use HTTPS and point at tag
   url "https://github.com/negadras/tada.git",
       using: :git,
-      tag:      "0.0.1"
+      tag:      "0.0.2"
 
   head "https://github.com/negadras/tada.git", branch: "main"
 
